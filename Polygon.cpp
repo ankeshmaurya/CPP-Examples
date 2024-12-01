ac360b3f6ef1cd9c7fe5f751fc9a6ee2bd0b9115
Ankesh Maurya <148422642+ankeshmaurya@users.noreply.github.com>
2024-12-01 21:25:33 +0530

    Add files via upload

---
+++ Polygon.cpp
@@ -0,0 +1,12 @@
+#include <simplecpp>
+main_program{
+    turtleSim();
+    cout << "How many sides?";
+    int nsides;
+    cin >> nsides;
+    repeat(nsides){
+    forward(100);
+    right(360.0/nsides);
+    }
+    wait(10);
+}
